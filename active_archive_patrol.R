c5d20ad0b38f06de9d69dcdee79afe83f7aae29a
Ozan Aygun <ozan@broadinstitute.org>
2017-04-29 08:42:44 -0400

    Continue to design of the archive script

--- active_archive_patrol.R
+++ active_archive_patrol.R
@@ -2,6 +2,8 @@
 # Designed to monitor Proteomics Active Archive
 ###############################################################################
 
+# Cat: date an time of the scan attempt for log
+
 # Check if the active.archive.rds file exists
 
         # If does not exist, parse the active.archive.rds file
@@ -12,10 +14,19 @@
                     # FUNCTION(x){ 
                     # Loop over these files
                         # Make sure they are real raw. files, not pseudo images
+                                # Cat: if files will be ignored make a log of them
                         # If they are real raw files; Extract file name and time stamp
+                                # Cat : if the files will be scanned make a log of them
                     # Exit the loop        
                     # Concatenate the file.name and time stamp information
                     # Extract Instrument,User,Weekday,LC label,Make a call for status: Operational or Downtime
                     # Hold and returns this as temp.active.archive  }
 
-        # Save temp.active.archive as active.archive.rds 
+                    # Save temp.active.archive as active.archive.rds into working directory
+                    # Push active.archive.rds into dropbox API
+
+
+
+
+# Create a scan time object
+# Push scan time object into dropbox API
