b92ec4d75463de561f73e41f8149a4fd3ba82cd1
Ozan Aygun <ozan@broadinstitute.org>
2017-04-30 20:05:43 -0400

    Directory search design

--- active_archive_patrol.R
+++ active_archive_patrol.R
@@ -1,14 +1,44 @@
 ###############################################################################
 # Designed to monitor Proteomics Active Archive
 ###############################################################################
+require(dplyr)
+require(tidyr)
+require(lubridate)
+require(rdrop2)
 
 # Cat: date an time of the scan attempt for log
+print(Sys.time())
+setwd("C:/Users/ozan/Desktop/Ozan_R_utilities/Development/JurkatQCscraper/active_archive_patrol/Active-archive-patrol")
+old_dir <- getwd()
+d<-paste(old_dir,"/Logs/Log_files_",Sys.Date(),sep = "") 
+if(!exists(d)){dir.create(d)}
 
 # Check if the active.archive.rds file exists
-# If exists, archive.file = readRDS(active.archive.rds)
-# If does not exist, archive.file = NULL
+if(file.exists("active.archive.rds")){
+        # If exists, archive.file = readRDS(active.archive.rds)
+        archive.file <- readRDS(active.archive.rds)
+}else{
+        # If does not exist, archive.file = NULL
+        archive.file <- NULL
+}
 
-                # Scan the active archive directory
+
+directories <- c("//flynn-cifs/proteomics_active_archive/Beaker_archive",
+                 "//flynn-cifs/proteomics_active_archive/Beaker2Comp_Archive",
+                 "//flynn-cifs/proteomics_active_archive/Curie_archive",
+                 "//flynn-cifs/proteomics_active_archive/Franklin_archive",
+                 "//flynn-cifs/proteomics_active_archive/galileo2_archive",
+                 "//flynn-cifs/proteomics_active_archive/galileo_archive",
+                 "//flynn-cifs/proteomics_active_archive/Hubble_archive",
+                 "//flynn-cifs/proteomics_active_archive/Hubble2_archive",
+                 "//flynn-cifs/proteomics_active_archive/McClintock_archive",
+                 "//flynn-cifs/proteomics_active_archive/Tesla_archive",
+                 "//flynn-cifs/proteomics_active_archive/Yoda_archive")
+
+# Scan the active archive directory
+
+
+                
                 # Search for .raw files, determine the list of raw files
                 # raw.file.list = list of new raw files
                     
